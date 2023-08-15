b97bb8731d26521b2aad8306532a30aaf04a318a
Vinicius Pansan <vinicius.spansan@gmail.com>
2023-08-15 16:21:40 -0300

    Initial Commit

---
+++ SupportApp/SupportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportAppApp.swift
+//  SupportApp
+//
+//  Created by Vinicius Serpa on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SupportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
